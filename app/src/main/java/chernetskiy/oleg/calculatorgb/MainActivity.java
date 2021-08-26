31ed0f85201beee9b1ca60b3acc306e37ca6db5f
Oleg <chernetskiyoleg09@gmail.com>
2021-08-26 12:32:59 +0300

    Сделал обработку кнопок с цифрами и логику калькулятора

--- app/src/main/java/chernetskiy/oleg/calculatorgb/MainActivity.java
+++ app/src/main/java/chernetskiy/oleg/calculatorgb/MainActivity.java
@@ -1,14 +1,26 @@
 package chernetskiy.oleg.calculatorgb;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textField = findViewById(R.id.text_field);
+    }
+
+    public void onNumberClick(View v) {
+        Button button = (Button) v;
+        textField.append(button.getText());
     }
 }
---
+++ app/src/main/java/chernetskiy/oleg/calculatorgb/PerformOperation.java
@@ -0,0 +1,5 @@
+package chernetskiy.oleg.calculatorgb;
+
+public interface PerformOperation {
+    void performOperation(Double number, String operation);
+}
