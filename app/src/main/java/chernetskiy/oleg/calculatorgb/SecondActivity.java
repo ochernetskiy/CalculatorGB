6260b5954053c9372b10aa7824ca9cf0a4f4df4f
Oleg <chernetskiyoleg09@gmail.com>
2021-09-03 22:35:06 +0300

    New commit

---
+++ app/src/main/java/chernetskiy/oleg/calculatorgb/SecondActivity.java
@@ -0,0 +1,26 @@
+package chernetskiy.oleg.calculatorgb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+import chernetskiy.oleg.calculatorgb.ui.MainActivity;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        findViewById(R.id.main_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
